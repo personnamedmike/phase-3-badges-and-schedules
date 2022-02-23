772e4f9fda53b47896d86cf7c900afb96b676b07
Mike Starr <imhs@protonmail.com>
2022-02-23 15:25:09 -0800

    commit tree b8050e91adaedb177608693adfbaa7d15bfe1b68

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
 # Write your code here.
+def badge_maker name
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator arr_names
+    arr_names.map {|name| "Hello, my name is #{name}."}
+end
+
+def assign_rooms speakers
+    roooms = 
+    speakers.each_with_index { |speaker, index| rooms << "Hello, #{speaker}! You'll be assigned to room #{index}!"}
+    roooms
+end
+
+def printer
+
+end
